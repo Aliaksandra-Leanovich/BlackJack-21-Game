41176f9af9b8d64987673aa86a2964d0b5ad58a9
«aleksandrics» <aliaksandraleanovich@gmail.com>
2022-11-15 12:31:32 +0300

    feat(game): add point count  for player and dealer

--- src/components/GameStart/GameStart.tsx
+++ src/components/GameStart/GameStart.tsx
@@ -1,28 +1,64 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "../../App.css";
 import { ICard } from "../../store/types";
 import { Button } from "../Button";
 import { useNavigate } from "react-router-dom";
 import { useAppSelector } from "../../store/hooks/hooks";
-import { getDeckId } from "../../store/selectors/deckIdSelectors";
+import {
+  getDeckId,
+  getDeckIdStatus,
+} from "../../store/selectors/deckIdSelectors";
 import { cardsApi } from "../../services/CardsService";
+import { Spinner } from "../Spinner";
 
 const GameStart = () => {
   const navigate = useNavigate();
   const { deckId } = useAppSelector(getDeckId);
-  const [cards, setCards] = useState<ICard[]>([]);
+  const status = useAppSelector(getDeckIdStatus);
+  const [cardsForPlayer, setCardsForPlayer] = useState<ICard[]>([]);
+  const [cardsForDealer, setCardsForDealer] = useState<ICard[]>([]);
+  const [countPlayer, setCountPlayer] = useState(0);
+  const [countDealer, setCountDealer] = useState(0);
 
   const handleBack = () => {
     navigate(-1);
   };
 
-  const getNewCard = async () => {
+  useEffect(() => {
+    countPoints(cardsForDealer, setCountDealer);
+    countPoints(cardsForPlayer, setCountPlayer);
+  }, [cardsForDealer, cardsForPlayer]);
+  const countPoints = (cards: ICard[], setPoints: (points: number) => void) => {
+    let points = 0;
+    cards.map((card: ICard) => {
+      switch (card.value) {
+        case "KING":
+          points = points + 4;
+          break;
+        case "ACE":
+          points = points + 11;
+          break;
+        case "JACK":
+          points = points + 2;
+          break;
+        case "QUEEN":
+          points = points + 3;
+          break;
+        default:
+          points = +card.value + points;
+      }
+    });
+    setPoints(points);
+  };
+
+  const getNewCard = async (cards: any, setCards: any) => {
     const api = await cardsApi.getCard(deckId);
     setCards(cards.concat(api));
   };
 
   const onSubmit = () => {
-    getNewCard();
+    getNewCard(cardsForPlayer, setCardsForPlayer);
+    getNewCard(cardsForDealer, setCardsForDealer);
   };
 
   return (
@@ -31,13 +67,24 @@ const GameStart = () => {
       <Button type="submit" handleClick={onSubmit}>
         New Card
       </Button>
-
-      {cards.map((card) => (
-        <li key={card.code}>
-          <p>{card.value}</p>
-          <img src={card.image} alt={card.code} className="card" />
-        </li>
-      ))}
+      <div>
+        <p>Player's points: {countPlayer}</p>
+        <p>Dealer's points: {countDealer}</p>
+      </div>
+      <div style={{ display: "flex" }}>
+        {cardsForPlayer.map((card) => (
+          <li key={card.code}>
+            <p>{card.value}</p>
+            <img src={card.image} alt={card.code} className="card" />
+          </li>
+        ))}
+        {cardsForDealer.map((card) => (
+          <li key={card.code}>
+            <p>{card.value}</p>
+            <img src={card.image} alt={card.code} className="card" />
+          </li>
+        ))}
+      </div>
     </div>
   );
 };
