742c6781e921877181f285568a8221352d280f74
«aleksandrics» <aliaksandraleanovich@gmail.com>
2022-11-23 14:36:46 +0300

    feat(app): add localstorage and fix inputs

--- src/components/Input/types.ts
+++ src/components/Input/types.ts
@@ -1,21 +1,22 @@
 import { ChangeEvent, InputHTMLAttributes } from "react";
-import { FieldError, FieldValues } from "react-hook-form";
-import { Path, UseFormRegister } from "react-hook-form";
-import { IFormInput } from "../SignUpForm/types";
+import {
+  FieldError,
+  FieldValues,
+  Path,
+  UseFormRegister,
+} from "react-hook-form";
 
-interface IFormValues {
-  email?: string;
+export interface IFormValues {
+  email: string;
   password?: string;
-  name?: string;
   errors?: string;
-  bet?: string;
 }
 
-interface IRegister extends FieldValues, IFormValues {}
+export interface IRegister extends FieldValues, IFormValues {}
 
 export interface IInputProps extends InputHTMLAttributes<HTMLInputElement> {
   label: Path<IFormValues>;
-  register: UseFormRegister<IFormInput> | UseFormRegister<FieldValues>;
+  register: UseFormRegister<IRegister>;
   errors?: FieldError | undefined;
 
   onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
