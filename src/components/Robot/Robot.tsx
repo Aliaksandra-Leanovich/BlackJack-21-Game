416eae2f6298f25e8d0c911e97140fc2959db2d8
«aleksandrics» <aliaksandraleanovich@gmail.com>
2022-11-30 14:46:18 +0300

    fix(app): fix mistakes

--- src/components/Robot/Robot.tsx
+++ src/components/Robot/Robot.tsx
@@ -4,16 +4,16 @@ export const Robot = () => {
   return (
     <div className={`${styles.robot} ${styles.bounce}`}>
       <div className={styles.circle}>
-        <div className={`${styles.robot__ear} ${styles.left}`}></div>
-        <div className={styles.robot__head}>
-          <div className={styles.robot__face}>
+        <div className={`${styles.ear} ${styles.left}`}></div>
+        <div className={styles.head}>
+          <div className={styles.face}>
             <div className={`${styles.eyes} ${styles.left}`}></div>
             <div className={`${styles.eyes} ${styles.right}`}></div>
             <div className={styles.mouth}></div>
           </div>
         </div>
-        <div className={`${styles.robot__ear} ${styles.right}`}></div>
-        <div className={styles.robot__body}></div>
+        <div className={`${styles.ear} ${styles.right}`}></div>
+        <div className={styles.body}></div>
       </div>
     </div>
   );
