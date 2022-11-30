09d56471fe26dafddbac130b14bc8f292db44360
«aleksandrics» <aliaksandraleanovich@gmail.com>
2022-11-30 13:48:39 +0300

    feat(app): add normalize css

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
+import "normalize.css";
 import "./index.module.scss";
 import store from "./store/store";
 
