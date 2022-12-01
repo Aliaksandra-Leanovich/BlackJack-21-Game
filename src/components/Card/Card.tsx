6a50668737dae9c9b32cd8a3a55a20843ecd1bfb
«aleksandrics» <aliaksandraleanovich@gmail.com>
2022-12-01 10:33:48 +0300

    fix(card): fix styles

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -3,8 +3,8 @@ import { IProps } from "./types";
 
 export const Card = ({ card }: IProps) => {
   return (
-    <div>
-      <img src={card.image} alt={card.code} className={styles.card} />
+    <div className={styles.card}>
+      <img src={card.image} alt={card.code} className={styles.image} />
     </div>
   );
 };
