9faa851c456ac943742aea7662cec622cde2e147
«aleksandrics» <aliaksandraleanovich@gmail.com>
2022-12-02 11:35:45 +0300

    fix(app): removed comment

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
-// import "normalize.css";
+import "normalize.css";
 import "./Main.module.scss";
 import store from "./store/store";
 
