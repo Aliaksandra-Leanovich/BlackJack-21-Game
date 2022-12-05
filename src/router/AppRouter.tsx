948f0381d6833b9671543afe227a6e4a0099fa3c
«aleksandrics» <aliaksandraleanovich@gmail.com>
2022-12-05 12:13:56 +0300

    feat(routes): change method of protexted routes

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,35 +1,23 @@
-import { Route, Routes, Navigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { MainTemplate } from "../components/MainTemplate/MainTemplate";
 import { Account } from "../pages/Account";
 import { BlackJack } from "../pages/BlackJack";
 import { Home } from "../pages/Home/Home";
 import { SignUp } from "../pages/SignUp";
 import { routes } from "../routes/routes";
-import { useAppSelector } from "../store/hooks";
-import { getUserInfo } from "../store/selectors";
+import { PrivateRoute } from "../utils/PrivateRoute";
 
 export const AppRouter = () => {
-  const { isAuthorized } = useAppSelector(getUserInfo);
   return (
     <Routes>
       <Route path={routes.HOME} element={<MainTemplate />}>
-        <Route
-          index
-          element={!isAuthorized ? <Navigate to={routes.SIGNUP} /> : <Home />}
-        />
+        <Route element={<PrivateRoute />}>
+          <Route index element={<Home />} />
+          <Route path={routes.BLACKJACK} element={<BlackJack />} />
+          <Route path={routes.ACCOUNT} element={<Account />} />
+        </Route>
+
         <Route path={routes.SIGNUP} element={<SignUp />} />
-        <Route
-          path={routes.BLACKJACK}
-          element={
-            !isAuthorized ? <Navigate to={routes.SIGNUP} /> : <BlackJack />
-          }
-        />
-        <Route
-          path={routes.ACCOUNT}
-          element={
-            !isAuthorized ? <Navigate to={routes.SIGNUP} /> : <Account />
-          }
-        />
       </Route>
     </Routes>
   );
