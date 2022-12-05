6ae33ab06d5c585e081eae3643946f4a4672340e
«aleksandrics» <aliaksandraleanovich@gmail.com>
2022-12-05 14:19:21 +0300

    feat(game): change the way of adding colors

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,23 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import preload from "vite-plugin-preload";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), preload()],
   envPrefix: "CH_",
   envDir: "./",
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `$primaryColor: #000000;
+        $secondaryColor: #343a40;
+        $secondaryLightColor: #6c757d;
+        $errorColor: #ffd166;
+        $infoColor: #cedbdd;
+        $infoLightColor: #ffffff;
+        $bg-color: #ff006e;
+        $robotTongueColor: #f9bbbb;`,
+      },
+    },
+  },
 });
